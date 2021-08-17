5d5663136e15f1a70fe94418abe43a6d20247cc6
mathieu abbal <mathieuabbal@hotmail.com>
2021-08-17 16:02:21 +0200

    responsive

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//modale header
 let toggleBtn = document.querySelector("#navbar-toggle");
 let collapse = document.querySelector("#navbar-collapse");
 
@@ -5,46 +6,12 @@ toggleBtn.onclick = () => {
   collapse.classList.toggle("hidden");
   collapse.classList.toggle("block");
 };
-
-function toggleModal(modalID) {
-  document.getElementById(modalID).classList.toggle("hidden");
-  document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
-  document.getElementById(modalID).classList.toggle("flex");
-  document.getElementById(modalID + "-backdrop").classList.toggle("flex");
-}
-
-var openmodal = document.querySelectorAll('.modal-open')
-for (var i = 0; i < openmodal.length; i++) {
-  openmodal[i].addEventListener('click', function(event){
-  event.preventDefault()
-  toggleModal()
-  })
-}
+//affiche année en cour
+var today=new Date();
+		var annee = today.getFullYear();
+		var sannee = document.getElementById('s_annee');
+		sannee.innerHTML = annee;
 
 
-var closemodal = document.querySelectorAll('.modal-close')
-for (var i = 0; i < closemodal.length; i++) {
-  closemodal[i].addEventListener('click', toggleModal)
-}
-
-document.onkeydown = function(evt) {
-  evt = evt || window.event
-  var isEscape = false
-  if ("key" in evt) {
-  isEscape = (evt.key === "Escape" || evt.key === "Esc")
-  } else {
-  isEscape = (evt.keyCode === 27)
-  }
-  if (isEscape && document.body.classList.contains('modal-active')) {
-  toggleModal()
-  }
-};
 
 
-function toggleModal () {
-  const body = document.querySelector('body')
-  const modal = document.querySelector('.modal')
-  modal.classList.toggle('opacity-0')
-  modal.classList.toggle('pointer-events-none')
-  body.classList.toggle('modal-active')
-}
